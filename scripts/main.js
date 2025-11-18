8f0b55ce3bcab5f573753b61b4272d8233cbed6f
bonillml <144627582+bonillml@users.noreply.github.com>
2025-11-17 21:13:23 -0500

    Updated Body Sections

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+/*
+Author: Matt Bonilla
+*/
+
+var globe = globe || {};
+
+globe.mainPageController = class
+{
+    constructor()
+    {
+
+    }
+}
+
+/* Main */
+/** function and class syntax examples */
+globe.main = function () 
+{
+    new globe.HomePageController();
+};
+
+globe.main();
